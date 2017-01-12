6b93ae4e7000c27ed17c8c784b73d0b8c2bdd1f1
Ewout Prangsma <ewout@prangsma.net>
2017-01-12 20:43:33 +0100

    Add google DNS to nodes

--- service/env/env.go
+++ service/env/env.go
@@ -16,11 +16,15 @@ package env
 
 import (
 	"os"
+	"strings"
 
 	"github.com/juju/errgo"
 
+	"io/ioutil"
+
 	"github.com/pulcy/gluon/service"
 	"github.com/pulcy/gluon/templates"
+	"github.com/pulcy/gluon/util"
 )
 
 var (
@@ -30,6 +34,7 @@ var (
 const (
 	bashrcTemplate = "templates/env/bashrc.tmpl"
 	bashrcPath     = "/home/core/.bashrc"
+	resolvConf     = "/etc/resolv.conf"
 
 	fileMode = os.FileMode(0644)
 )
@@ -46,6 +51,9 @@ func (t *envService) Name() string {
 
 func (t *envService) Setup(deps service.ServiceDependencies, flags *service.ServiceFlags) error {
 	if err := createBashrc(deps, flags); err != nil {
+		return maskAny(err)
+	}
+	if err := addGoogleDNS(deps, flags); err != nil {
 		return maskAny(err)
 	}
 
@@ -66,5 +74,29 @@ func createBashrc(deps service.ServiceDependencies, flags *service.ServiceFlags)
 		return maskAny(err)
 	}
 
+	return nil
+}
+
+func addGoogleDNS(deps service.ServiceDependencies, flags *service.ServiceFlags) error {
+	content, err := ioutil.ReadFile(resolvConf)
+	if os.IsNotExist(err) {
+		content = []byte("")
+	} else if err != nil {
+		return maskAny(err)
+	}
+	dnsLine := "nameserver 8.8.8.8"
+	lines := strings.Split(string(content), "\n")
+	for _, l := range lines {
+		if strings.TrimSpace(l) == dnsLine {
+			// Google DNS already found
+			return nil
+		}
+	}
+	// Append google dns
+	lines = append(lines, dnsLine)
+	content = []byte(strings.Join(lines, "\n") + "\n")
+	if _, err := util.UpdateFile(deps.Logger, resolvConf, content, 0755); err != nil {
+		return maskAny(err)
+	}
 	return nil
 }
